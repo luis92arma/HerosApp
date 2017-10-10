199601a5ec9064be123bce5573ad8d7e31d823d6
luis92arma <luis.arma92@gmail.com>
2017-10-10 13:29:21 -0500

    basic service local data :trollface:

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
+
+  constructor(private _heroesService:HeroesService) {
+    console.log('Contrullendo...');
+   }
 
   ngOnInit() {
+    console.log('Init');
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
