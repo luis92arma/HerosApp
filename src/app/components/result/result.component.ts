ed29351ca9bea2a9276a87aae496fc01845d1f75
luis92arma <luis.arma92@gmail.com>
2017-10-11 17:39:04 -0500

    buscador

---
+++ src/app/components/result/result.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+//import { NavbarComponent } from '../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styles: []
+})
+export class ResultComponent implements OnInit {
+
+  heroes:Heroe[] = []
+
+  constructor(
+    private _heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    //this.heroes = this.resultadoHeroes.busquedaHeroe;
+    this.activatedRoute.params.subscribe(params =>{
+      console.log(params);
+      this.heroes = this._heroesService.buscarHeroes(params.heroe);
+      console.log(this.heroes)
+    })
+  }
+
+}
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  buscarHeroe(heroe:string){
+    //console.log(heroe);
+    this.router.navigate(['../busqueda', heroe])
   }
 
 }
